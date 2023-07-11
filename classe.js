045fb3442b9c656bfbae640cd1bb6115ebc5d9a2
Everton Figueiredo <j.everton.figueiredo@gmail.com>
2023-07-10 21:12:58 -0300

    Splice da Class

---
+++ classe.js
@@ -0,0 +1,46 @@
+class Pedido {
+    constructor() {
+        this.itens = []
+        this.total = 0
+    }
+
+    addItem(codigo, qtd = 0, descricao = "", valorUnit = 0) {
+        let item = {
+            codigo,
+            descricao,
+            qtd,
+            valorUnit,
+            valorTotal: qtd * valorUnit
+        }
+        this.itens.push(item)
+        this.calcularTotalNota()
+    }
+
+    calcularTotalNota() {
+        this.total = 0
+        this.itens.map(item => {
+            this.total += item.valorTotal
+        })
+    }
+
+    removerItem(codigo){
+        if (codigo != '') {
+
+            let resultado = this.itens.findIndex(item => item.codigo === codigo)
+
+            if (resultado == -1) {
+                console.log("Item não encontrado na Nota");
+                return
+            }
+
+            this.itens.splice(resultado, 1)
+            console.log(`Item de codigo: ${codigo} removido da nota!`);
+            this.calcularTotalNota()
+        } else {
+            console.log("Passe um codigo valido!!");
+        }
+    }
+
+}
+
+let novoPedido = new Pedido()
